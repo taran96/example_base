8aa1beac68b3cc51976b8f1a65b38e0c48f0349b
Taranvir Waraich <taranvir@me.com>
2018-03-28 21:46:37 -0400

    Added the foreign key to color from the user model

--- users/models.py
+++ users/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+from colors.models import Color
+
 
 class User(AbstractUser):
-    pass  # Get rid of this when you add the missing field
+    color = models.ForeignKey(Color, on_delete=models.SET_NULL, null=True)
